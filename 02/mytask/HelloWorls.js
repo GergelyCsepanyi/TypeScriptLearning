9a501e020ee237edf0eca6c79d4dd7ffc0610a2b
Gergely Laszlo Csepanyi <gergely-laszlo.csepanyi@nixs.com>
2023-03-24 17:48:28 +0100

    Revert "Learning and practicing in TypeScript."
    This reverts commit 98c2a0efd2a2d40967493f9d034d32d0a14c5bfc.

--- 02/mytask/HelloWorls.js
+++ 02/mytask/HelloWorls.js
@@ -3,7 +3,3 @@ const sayHello = (name) => {
     console.log(`Hello ${name}!`);
 };
 sayHello("Test");
-const greet = (name, age) => {
-    console.log(`Hello ${name}, ${age}`);
-};
-greet("Bob", 30);
